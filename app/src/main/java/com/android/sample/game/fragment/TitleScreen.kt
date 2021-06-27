cd57c0d332ca388945396ab3dcf74e84718c78b8
Ali Rezaei <ali.rezaei09@gmail.com>
2021-06-27 19:58:30 +0430

    Bind orientation to portrait mode

--- app/src/main/java/com/android/sample/game/fragment/TitleScreen.kt
+++ app/src/main/java/com/android/sample/game/fragment/TitleScreen.kt
@@ -16,6 +16,7 @@ class TitleScreen : DaggerFragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_title_screen, container, false)
 
         view.findViewById<Button>(R.id.play_btn).setOnClickListener {
